3516ae6ef9dce05de7b63a6d5722fca85a32fc51
Francisco Lima <36736375+franciscojml@users.noreply.github.com>
2019-09-17 17:01:10 -0300

    componente-lista-tarefas

---
+++ projeto-02-tarefas/src/components/TaskGrid.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="tasks">
+        <template>
+            <div v-for="task in tasks" :key="task.name">
+                {{task.name}}
+            </div>
+        </template>
+    </div>
+    </template>
+<script>
+export default {
+    props:{
+        tasks: {type: Array, require: true}
+    }
+}
+</script>
+<style >
+
+</style>
