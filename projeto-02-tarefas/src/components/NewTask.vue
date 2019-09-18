f18d3ee41e911827d8fa8013963353dc33b647af
Francisco Lima <36736375+franciscojml@users.noreply.github.com>
2019-09-18 11:29:29 -0300

    componente-nova-tarefa02

--- projeto-02-tarefas/src/components/NewTask.vue
+++ projeto-02-tarefas/src/components/NewTask.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="task-grid">
-        <input v-model="name" type="text" @keydown.enter="add">
-        <button @click="add">+</button>
+    <div class="new-task">
+        <input v-model="name" type="text" @keydown.enter="add" class="form-element" placeholder="Nova tarefa?">
+        <button @click="add" class="form-element">+</button>
     </div>
 </template>
 <script>
@@ -21,5 +21,29 @@ export default {
 }
 </script>
 <style >
+    .new-task {
+        margin: 35px;
+    }
 
+    .form-element {
+        outline: none;
+        font-size: 2rem;
+        border: 1px solid white;
+        padding: 5px 10px 8px;
+        color: white;
+    }
+
+    input.form-element {
+        width: 500px;
+        background: #FFF2;
+        border-top-left-radius: 8px;
+        border-bottom-left-radius: 8px;
+    }
+
+    button.form-element {
+        border-left: none;
+        background-color: #2196F3;
+        border-top-right-radius: 8px;
+        border-bottom-right-radius: 8px;
+    }
 </style>
