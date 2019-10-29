b3378bc489167a71ba8adf4e4914e3a8179784b4
Francisco Lima <36736375+franciscojml@users.noreply.github.com>
2019-10-29 14:53:18 -0300

    rotas

---
+++ rotas/rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="usuario-detalhe">
+        <h3>Usuario Detalhe</h3>
+        <hr>
+        <p><strong>Código:</strong> {{ id }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    /*data(){
+        return{
+            id: this.$route.params.id
+        }
+    },
+    watch: {
+        $route(to, from){
+            this.id = to.params.id
+        }
+    },*/
+    props: [ 'id' ],
+}
+</script>
+
+<style>
+
+</style>
---
+++ rotas/rotas-exercicios/src/components/usuario/UsuarioEditar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="usuario-editar">
+        <h3>Usuario Detalhe</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    /*data(){
+        return{
+            id: this.$route.params.id
+        }
+    },
+    watch: {
+        $route(to, from){
+            this.id = to.params.id
+        }
+    },*/
+    props: [ 'id' ],
+}
+</script>
+
+<style>
+
+</style>
