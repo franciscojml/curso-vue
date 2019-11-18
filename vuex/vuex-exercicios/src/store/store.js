aae79a3fc545e714e9cdfc38cd216891cbb2d917
Francisco Lima <36736375+franciscojml@users.noreply.github.com>
2019-11-18 09:46:16 -0300

    vuex-start

---
+++ vuex/vuex-exercicios/src/store/store.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import carrinho from '@/store/modules/carrinho'
+import parametros from '@/store/modules/parametros'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    modules: { carrinho, parametros }
+})
